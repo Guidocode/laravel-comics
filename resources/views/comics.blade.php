2e07426660d6129e389f0d42b0b0b78708fef579
Guidocode <guidobuono@icloud.com>
2022-06-23 18:57:48 +0200

    Jumbo e nav blue

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -2,8 +2,12 @@
 
 @section('content')
 
+    <div class="jumbo"></div>
+
+
     <main>
 
+        {{-- Comics --}}
         <!-- Titolo contenitore -->
         <div class="gb-container">
         <div class="title-content">current series</div>
@@ -25,9 +29,61 @@
         </div>
 
 
-        <div class="gb-container  text-center">
+        <div class="gb-container  text-center mb-5">
             <button type="button" class="btn btn-primary gb-btn">Load More</button>
         </div>
+        {{-- /Comics --}}
+
+
+
+
+        {{-- Light Blue Band --}}
+        <div class="light-blue-band py-5">
+            <div class="gb-container">
+              <ul class="d-flex justify-content-between m-0 px-5">
+
+                <!-- Voci menu -->
+                <li>
+                  <a class="d-flex align-items-center" href="#">
+                    <div class="icon"
+                    style="background-image: url('{{ asset('img/buy-comics-digital-comics.png')}}');"></div>
+                    <span>digital comics</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="d-flex align-items-center" href="#">
+                    <div class="icon"
+                    style="background-image: url('{{ asset('img/buy-comics-merchandise.png')}}');"></div>
+                    <span>dc merchandise</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="d-flex align-items-center" href="#">
+                    <div class="icon"
+                    style="background-image: url('{{ asset('buy-comics-subscriptions.png')}}');"></div>
+                    <span>subscription</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="d-flex align-items-center" href="#">
+                    <div class="icon"
+                    style="background-image: url('{{ asset('img/buy-comics-shop-locator.png')}}');"></div>
+                    <span>comics shop locator</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="d-flex align-items-center" href="#">
+                    <div class="icon"
+                    style="background-image: url('{{ asset('img/buy-dc-power-visa.svg')}}');"></div>
+                    <span>dc power visa</span>
+                  </a>
+                </li>
+                <!-- /Voci menu -->
+
+              </ul>
+            </div>
+          </div>
+        {{-- /Light Blue Band --}}
 
     </main>
 
