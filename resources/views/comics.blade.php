bf4b4cad1d5707dfd59387a74f27b131fad7b79c
Guidocode <guidobuono@icloud.com>
2022-06-23 17:08:39 +0200

    Fumetti stilati

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -2,15 +2,33 @@
 
 @section('content')
 
-    <h3>Comics</h3>
+    <main>
 
-    @foreach ($comics as $comic)
-    <div class="gb-card">
-        <div class="image-card">
-          <img src="{{$comic->image}}" alt="">
+        <!-- Titolo contenitore -->
+        <div class="gb-container">
+        <div class="title-content">current series</div>
         </div>
-        <div class="title-card">{{$comic->title}}</div>
-    </div>
-    @endforeach
+        <!-- /Titolo contenitore -->
+
+
+        <div class="gb-container container-cards d-flex flex-wrap">
+
+            @foreach ($comics as $comic)
+            <div class="gb-card">
+                <div class="image-card">
+                <img src="{{$comic->image}}" alt="{{$comic->title}}">
+                </div>
+                <div class="title-card">{{$comic->title}}</div>
+            </div>
+            @endforeach
+
+        </div>
+
+
+        <div class="gb-container  text-center">
+            <button type="button" class="btn btn-primary gb-btn">Load More</button>
+        </div>
+
+    </main>
 
 @endsection
